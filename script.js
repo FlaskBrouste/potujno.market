46ce5448f3f66df7da35d8977d95607596dd950d
blacksempai <benjaminfranclin1998@gmail.com>
2021-11-23 15:57:53 +0200

    Add adaptivity to cart

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ xhr.onload = function() {
         pElem.innerHTML = `
             <h2 class='product-name'>${p.name}</h2>
             <img class='product-photo' src='${p.photo_url}' alt='${p.name}'>
-            <p class='product-price'><b>Price: </b>${p.price} $</p>
+            <p class='product-price'><b>Price: </b>${p.price}$</p>
             <p class='product-description'><b>Description: </b>${p.description}</p>
             <a href='userProfile.html?id=${p.author_id}'>Go to author profile</a>
             <button onclick="addProductToCart(${p.id})">Buy</button>
@@ -51,19 +51,19 @@ function drawCartProducts() {
     let sum = 0;
     cart.forEach(function(p){
         cartProd.innerHTML += `
-            <p>${p.name} | ${p.price} $</p>
+            <p><img src="${p.photo_url}" height="30px"> ${p.name} |${p.price}$</p>
         `;
         sum += p.price;
     });
     cartProd.innerHTML += `
-        <p>Total Price: ${sum} $</p>
+        <p>Total Price: ${sum}$</p>
         <button onclick="buyAll()">Buy All</button>
     `
 }
 
 function buyAll() {
     cart = [];
-    cartProd.innerHTML = 'Money was withdrawn from your credit card >=)'
+    cartProd.innerHTML = 'Money was withdrawn from your credit card'
 }
 
 function openCart() {
